d51e169f093c2d3fdd97445ea34d487450c97f16
aljohara <aljoharafawazaaa@gmail.com>
2020-06-12 20:10:41 +0300

    Added a loop that takes in the name of the students from the user

--- max8min.py
+++ max8min.py
@@ -1,1 +1,13 @@
-name = input("Hello! whats your name?")
+import array
+
+name = input("Hello! whats your name?\n")
+print("Hello " + name + " please enter the number of students"
+" You would like to calculate their maximum and their minimum")
+numOfStudents = int(input())
+listOfNames = array.array('u', [])
+for i in range(numOfStudents):
+
+    nameOfStudents = input("Whats the name of the " + str(i) + " student\n")
+    listOfNames.append(nameOfStudents)
+
+print(','.join(listOfNames))
