37d1d391e41b543d1ea391488232319dd2ad1fbe
aljohara <aljoharafawazaaa@gmail.com>
2020-06-14 02:23:53 +0300

    Small fixes

--- max8min.py
+++ max8min.py
@@ -1,9 +1,12 @@
 import array
 
 name = input("Hello! whats your name?\n")
+
 print("Hello " + name + " please enter the number of students"
 " You would like to calculate their maximum and their minimum")
+
 numOfStudents = int(input())
+
 listOfNames = array.array('u', [])
 for i in range(numOfStudents):
 
